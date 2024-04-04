cee27f8f472efeaecb4dd77a66bd7bf73b008edf
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-04-04 22:32:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from vllm import LLM, SamplingParams
 
 class InferlessPythonModel:
     def initialize(self):
-        model_id = "TheBloke/Llama-2-13B-chat-GPTQ"  # Specify the model repository ID
+        model_id = "Intel/neural-chat-7b-v3-1"  # Specify the model repository ID
         # Define sampling parameters for model generation
         self.sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=256)
         # Initialize the LLM object
